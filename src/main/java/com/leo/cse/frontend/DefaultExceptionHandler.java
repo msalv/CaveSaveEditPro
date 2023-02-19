e044e9b2a151a90ef4a09c8cfa4fb35d344d1ddd
msalv <bikovgta13@gmail.com>
2023-02-19 18:38:36 +0300

    Ask users if they want to restart the app in case of an unexpected error

--- src/main/java/com/leo/cse/frontend/DefaultExceptionHandler.java
+++ src/main/java/com/leo/cse/frontend/DefaultExceptionHandler.java
@@ -9,21 +9,27 @@ public class DefaultExceptionHandler implements UncaughtExceptionHandler {
 	@Override
 	public void uncaughtException(Thread t, Throwable e) {
 		AppLogger.fatal(String.format("Uncaught exception in thread %s", t.getName()), e);
-		JOptionPane.showMessageDialog(
+		final int choice = JOptionPane.showConfirmDialog(
 				null,
 				"An uncaught exception has occurred in thread " + t.getName() + ":\n"
 						+ e + "\n" +
 						"Please send the error log (\"cse.log\") to the developer,\n" +
-						"along with a description of what you did leading up to the exception.",
-				"Uncaught exception!",
+						"along with a description of what you did leading up to the exception.\n" +
+						"\nWould you like to restart the app?",
+				"Uncaught exception! Restart?",
+				JOptionPane.YES_NO_OPTION,
 				JOptionPane.ERROR_MESSAGE);
 
-		try {
-			// Do not load profile and game resources on restart
-			// to avoid same errors
-			Config.safeMode();
-			CaveSaveEdit.restart();
-		} catch (Exception ex) {
+		if (choice == JOptionPane.YES_OPTION) {
+			try {
+				// Do not load profile and game resources on restart
+				// to avoid same errors
+				Config.safeMode();
+				CaveSaveEdit.restart();
+			} catch (Exception ex) {
+				System.exit(1);
+			}
+		} else {
 			System.exit(1);
 		}
 	}
